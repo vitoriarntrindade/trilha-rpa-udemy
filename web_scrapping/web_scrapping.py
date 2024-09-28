147de8fca8a3e00126eeb7d67672c7826f8ef39c
Vitória <vitoria.raymara.tatix@gmail.com>
2024-09-28 14:09:27 -0300

    Section: Web Scrapping - aula 169 - Navegando entre páginas do mesmo site para extração dos dados.

--- web_scrapping/web_scrapping.py
+++ web_scrapping/web_scrapping.py
@@ -7,13 +7,23 @@ import pyautogui
 webdriver = webdriver.Chrome()
 webdriver.get("https://rpachallengeocr.azurewebsites.net/")
 
-element_table = webdriver.find_element(By.XPATH, '//*[@id="tableSandbox"]')
-
-lines_table = element_table.find_elements(By.TAG_NAME, "tr")
-columns_table = element_table.find_elements(By.TAG_NAME, "td")
-
 lines = 1
-for line in lines_table:
-    print(line.text)
-    lines =+ 1
 
+i = 1
+while i < 4:
+    element_table = webdriver.find_element(By.XPATH, '//*[@id="tableSandbox"]')
+
+    lines_table = element_table.find_elements(By.TAG_NAME, "tr")
+    columns_table = element_table.find_elements(By.TAG_NAME, "td")
+
+    for line in lines_table:
+        print(line.text)
+        lines =+ 1
+
+    i += 1
+
+    pyautogui.sleep(3)
+
+    webdriver.find_element(By.XPATH,'//*[@id="tableSandbox_next"]').click()
+
+    pyautogui.sleep(3)
