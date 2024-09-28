461296be71c3ac501cc2edb807f0d4b4388e453b
Vitória <vitoria.raymara.tatix@gmail.com>
2024-09-28 15:21:50 -0300

    Section: Web Scrapping - aula 170 - Navegando entre páginas do mesmo site para extração dos dados para uma planilha em Excel.

--- web_scrapping/web_scrapping.py
+++ web_scrapping/web_scrapping.py
@@ -1,29 +1,36 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import pyautogui
+import pyautogui as wait_time
+from openpyxl import load_workbook
+import os
+from typing import List
 
 
-webdriver = webdriver.Chrome()
-webdriver.get("https://rpachallengeocr.azurewebsites.net/")
+def extract_table_data_to_excel(file_name: str, url: str, sheet_name: str, iterations: int) -> None:
+    spreadsheet = load_workbook(filename=file_name)
+    selected_sheet = spreadsheet[sheet_name]
 
-lines = 1
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-i = 1
-while i < 4:
-    element_table = webdriver.find_element(By.XPATH, '//*[@id="tableSandbox"]')
+    for _ in range(iterations):
+        table = driver.find_element(By.XPATH, '//*[@id="tableSandbox"]')
+        rows = table.find_elements(By.TAG_NAME, "tr")
 
-    lines_table = element_table.find_elements(By.TAG_NAME, "tr")
-    columns_table = element_table.find_elements(By.TAG_NAME, "td")
+        for row_index, row in enumerate(rows, start=len(selected_sheet['A']) + 1):
+            columns: List[str] = row.text.split(" ")
+            if len(columns) >= 3:
+                selected_sheet[f"A{row_index}"] = columns[0]
+                selected_sheet[f"B{row_index}"] = columns[1]
+                selected_sheet[f"C{row_index}"] = columns[2]
 
-    for line in lines_table:
-        print(line.text)
-        lines =+ 1
+        wait_time.sleep(2)
+        next_button = driver.find_element(By.XPATH, '//*[@id="tableSandbox_next"]')
+        next_button.click()
+        wait_time.sleep(2)
 
-    i += 1
+    spreadsheet.save(filename=file_name)
+    driver.quit()
+    os.startfile(file_name)
 
-    pyautogui.sleep(3)
-
-    webdriver.find_element(By.XPATH,'//*[@id="tableSandbox_next"]').click()
-
-    pyautogui.sleep(3)
+extract_table_data_to_excel(file_name="data.xlsx", url="https://rpachallengeocr.azurewebsites.net/", sheet_name="Data", iterations=3)
